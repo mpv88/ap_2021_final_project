d13f4d7e44bc0739748475817351f42bbfe8dbdb
mpv_ubu <mattiapivi@gmail.com>
2022-09-03 15:32:07 +0200

    setup

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// RBT driver
+
+#include"RBT.hpp"
+
+
+int main() {
+  RBTree bst;
+  bst.insert(8);
+  bst.insert(18);
+  bst.insert(5);
+  bst.insert(15);
+  bst.insert(17);
+  bst.insert(25);
+  bst.insert(40);
+  bst.insert(80);
+  bst.deleteNode(25);
+  bst.prettyPrint();
+  return 0;
+}
