2830ebb9896b273935f5f64e7d42c7f86b8811a8
mpv_ubu <mattiapivi@gmail.com>
2022-09-25 17:25:08 +0200

    bmk

--- bmk/bmk.cpp
+++ bmk/bmk.cpp
@@ -1,4 +1,10 @@
-// Bmk test driver
+///\file bmk.cpp
+///\author mpv
+///\brief Bmk test driver
+/// Small benchmarking experiment to (I) measure and (II) compare the look-up performance of realized RBTree (contains() method) versus find() method of std::map and std::unordered_map.
+/// First, some randomly generated integer numbers have been created and inserted into those three containers, thus increasing linearly nodes' number -in tens increments- from 100 to 10000. 
+/// Secondly, the average time of retreiving 1000 random numbers from each container has been measured and finally plotted.
+/// Expectations are to find a better performance for std::unordered_map, as it is based on hash table while the other two (std::map and our RBTree) are based on red-black tree structures. 
 
 #include <chrono>
 #include <fstream>
@@ -7,68 +13,88 @@
 #include <random>
 #include <unordered_map>
 #include <vector>
-#include "RBT.hpp"
+#include "../include/RBT.hpp"
+
+///\brief function to generate random numbers (overloaded).
+///\param numbers number of random numbers to be generated.
+///\param is_double dummy to choose the type of random numbers (int or double).
+///\return a vector filled with random numbers of type chosen.
+std::vector<int> generate_random(const int& numbers) {
+  std::vector<int> v;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, numbers);
+  for (int i=0; i<numbers; i++) {
+      v.push_back(dis(gen));
+    }
+  return v;
+}
+
+std::vector<double> generate_random(const int& numbers, const bool& is_double) {
+  std::vector<double> v;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> dis(0, numbers);
+  for (int i=0; i<numbers; i++) {
+      v.push_back(double(dis(gen)));
+    }
+  return v;
+}
+
+///\brief function to measure the average time in microseconds to complete a search.
+///\param container container to be searched.
+///\param numbers vector of random numbers to be searched.
+///\param search_size initial number of elements to be found in the container.
+///\return average time in milliseconds.
+template <typename T>
+double measure_time(T& container, const std::vector<double>& numbers, const int& search_size) {
+  auto start = std::chrono::high_resolution_clock::now();
+  start = std::chrono::system_clock::now();
+  for (int i=0; i<search_size; ++i) {
+    container.find(numbers[i]);
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+  return elapsed.count()/double(search_size);
+}
+
 
 int main() {
+  // output stream and output file
+  std::ofstream out;
+  out.open("../bmk/bmk_measures_double.txt");
 
+  // define our experimental subjects (containers)
+  std::map<double, double> m;
+  std::unordered_map<double, double> um;
+  RBTree<double> rbt;
 
-    const int N{10000}; // Number of elements to be inserted and found 
+  // define the size of the experiment
+  int size_experiment{20000};
 
-    std::ofstream outFile;
-    outFile.open("../bmk/bmk_measures.txt");
+  // define initial size of the search within the containers
+  int size_search{50};
 
-    //For generating random numbers
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(1,N); 
+  // define the size of the increment in the size of the search
+  int step{50};
 
-    std::vector<int> numbers;
-    for(int j = 0; j <= N; ++j){
-      numbers.push_back(dist(rng));
+  // generate random numbers
+  //std::vector<int> numbers = generate_random(size_experiment);
+  std::vector<double> numbers = generate_random(size_experiment, 1);
+
+  // insert numbers into containers & measure time elapsed to find them
+  for (int i=size_search; i<=size_experiment; i+=step) {
+    for(int j=0; j<i; ++j){
+      m.insert({numbers[j],numbers[j]});
+      um.insert({numbers[j],numbers[j]});
+      rbt.insert({numbers[j]});
     }
+    // print the output times
+    out << i << "\t" << measure_time(m, numbers, i) << "\t" << measure_time(um, numbers, i) << "\t" << measure_time(rbt, numbers, i) << std::endl;
+  }
 
-    RBTree<int> rbt; // RedBlackTree
-    std::map<int,int> map_; // STD map
-    std::unordered_map<int,int> umap_; // STD unordered map
+  // close output file
+  out.close();
 
-
-for (size_t i = 100; i <= N; i+=10)
-{
-    for(size_t k = 0; k < i; ++k){
-        rbt.insert({numbers[k]});
-        map_.insert({numbers[k], numbers[k]});
-        umap_.insert({numbers[k], numbers[k]});
-    }  
-
-    //measuring time to find keys with std::map
-  	auto t0_map = std::chrono::steady_clock::now();
-   	for(size_t k = 0 ; k < i; ++k){
- 		  map_.find(numbers[k]);
-  	}
-	  auto t1_map = std::chrono::steady_clock::now();
-	  auto elapsed_map = std::chrono::duration_cast<std::chrono::microseconds>(t1_map-t0_map);  
-
-    
-    //measuring time to find keys with std::unordered_map
-  	auto t0_umap = std::chrono::steady_clock::now();
-   	for(size_t k = 0; k < i; ++k){
- 		  umap_.find(numbers[k]);
-  	}
-	  auto t1_umap = std::chrono::steady_clock::now();
-	  auto elapsed_umap = std::chrono::duration_cast<std::chrono::microseconds>(t1_umap-t0_umap);
-
-    //measuring time to find keys with rbt
-  	auto t0_rbt = std::chrono::steady_clock::now();
-   	for(size_t k{0}; k < i; ++k){
- 		  rbt.contains(numbers[k]);
-  	}
-	  auto t1_rbt = std::chrono::steady_clock::now();
-	  auto elapsed_rbt = std::chrono::duration_cast<std::chrono::microseconds>(t1_rbt-t0_rbt);
-
-    // write results to file
-    outFile << i << "\t" << (double(elapsed_map.count()))/((double)N) << "\t" << (double(elapsed_umap.count()))/((double)N) << "\t" << (double(elapsed_rbt.count()))/((double)N) << "\n";
-}
-	  outFile.close();
-	  return 0; 
-
+  return 0;
 }
--- bmk/bmk_times_plot.py
+++ bmk/bmk_times_plot.py
@@ -2,18 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # read the data file and load it into a pandas dataframe
-data = pd.read_csv('bmk_measures.txt', sep ='\t')
-data.columns = ['#nodes', 'std::map', 'std::unordered_map', 'RedBlackTree']
+data = pd.read_csv('bmk_measures_double.txt', sep ='\t')
+data.columns = ['#elements', 'std::map', 'std::unordered_map', 'RedBlackTree']
 
 # plot the three data series
 fig = plt.figure(figsize=(15, 10))
-plt.title('Benchmarking comparison')
-plt.xlabel('# nodes')
+plt.title('Benchmarking comparison (double)')
+plt.xlabel('# elements to be found')
 plt.ylabel('time (μs)')
 ax = plt.subplot(111)
-ax.plot(data['#nodes'], data['std::map'], label = 'map')
-ax.plot(data['#nodes'], data['std::unordered_map'], label = 'unordered_map')
-ax.plot(data['#nodes'], data['RedBlackTree'], label = 'RBTree')
+ax.plot(data['#elements'], data['std::map'], label = 'map')
+ax.plot(data['#elements'], data['std::unordered_map'], label = 'unordered_map')
+ax.plot(data['#elements'], data['RedBlackTree'], label = 'RBTree')
 ax.legend()
 
 
